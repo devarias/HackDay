7978babe4dec634683a2b69c523cdeb41eb1565a
devarias <gedafu2005@gmail.com>
2021-02-22 07:03:19 -0500

    fixed the how to use button

--- checker/src/Components/Footer/Footer.js
+++ checker/src/Components/Footer/Footer.js
@@ -1,18 +1,17 @@
 import React, { Component } from "react";
-const footerIcon = "https://i.imgur.com/TUdHBzr.png"
+const footerIcon = "https://i.imgur.com/TUdHBzr.png";
 
 class Footer extends Component {
   render() {
     return (
       <div className="footer">
         <p className="footer">
-          Made by valentina Jaramillo, Ricardo Arquimedes and David Arias - 
-          Colombia 2021 <br></br> May the 4th be with you</p>
-         <img className="footerIcon" src={footerIcon} alt="" />
-        
+          Made by valentina Jaramillo, Ricardo Arquimedes and David Arias -
+          Colombia 2021 <br></br> May the 4th be with you
+        </p>
+        <img className="footerIcon" src={footerIcon} alt="" />
       </div>
     );
   }
 }
 export default Footer;
-
