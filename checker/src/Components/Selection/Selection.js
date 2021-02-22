7baac54d53e7d8cb144545565306f2dc7172573f
RicardoArquimedes <richardcqt28@gmail.com>
2021-02-21 22:35:48 -0500

    updating styles selection

--- checker/src/Components/Selection/Selection.js
+++ checker/src/Components/Selection/Selection.js
@@ -5,6 +5,7 @@ import axios from "axios";
 
 function Selection() {
   const [projectNumber, setProjectNumber] = useState(0);
+  const bbEight = "https://i.pinimg.com/originals/6c/7d/a9/6c7da91893f7e628807a802ff423cf28.gif";
   const [tasks, setTasks] = useState(0);
 
   const handleGetProject = (event) => {
@@ -32,6 +33,7 @@ function Selection() {
   };
   return (
     <div>
+      <img className="bbEight" src={bbEight} alt="" />
       <input
         className="box"
         type="text"
