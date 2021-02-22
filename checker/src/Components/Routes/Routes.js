6e39d24c99f58315f93e6e5c513f8c290ed5c892
devarias <gedafu2005@gmail.com>
2021-02-21 20:44:07 -0500

    Revert "fixing routes"
    This reverts commit 8e6ddeebb351b7fc7d5ea10d411f1001b0fb7a32.

--- checker/src/Components/Routes/Routes.js
+++ checker/src/Components/Routes/Routes.js
@@ -3,7 +3,7 @@ import { observer } from "mobx-react";
 // npm i -S react-router-dom
 import { Router, Switch, Route } from "react-router-dom";
 import history from "../History/History";
-
+import Selection from "../Selection/Selection";
 import Login from "../Login/Login";
 
 class Routes extends Component {
@@ -12,7 +12,7 @@ class Routes extends Component {
       <Router history={history}>
         <Switch>
           <Route path="/" exact component={Login} />
-          <Route path="/selection" exact component={Login} />
+          <Route path="/selection" exact component={Selection} />
         </Switch>
       </Router>
     );
